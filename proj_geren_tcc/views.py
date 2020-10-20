7752dddfcaef63622f7cdd403e901555a4d39bf3
megazordcarioca <josiasdsj1@gmail.com>
2020-10-20 19:11:12 -0300

    Iniciando a organização dos componentes de aluno e telas de login

--- proj_geren_tcc/views.py
+++ proj_geren_tcc/views.py
@@ -10,3 +10,8 @@ def homepage(request):
 def about(request):
     # return HttpResponse('about')
     return render(request, 'about.html')
+
+
+def logon(request):
+    # return HttpResponse('about')
+    return render(request, 'login.html')
