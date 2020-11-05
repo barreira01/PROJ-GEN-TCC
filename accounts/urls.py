e9b99101b2f0b592c83cd31f6f88bbc149e4c964
megazordcarioca <josiasdsj1@gmail.com>
2020-11-04 22:48:27 -0300

    Remoção dos apps de coordenador e professor na v2

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from. import views
 app_name ='accounts'
 
 urlpatterns = [
-    url('signup/', views.signup_view, name="signup"),
-    url('login/', views.login_view, name="login"),
-    url('logout/', views.logout_view, name="logout")
+    #url('signup/', views.signup_view, name="signup"),
+    url('login/', views.login, name="login"),
+    url('logout/', views.logout, name="logout")
 ]
