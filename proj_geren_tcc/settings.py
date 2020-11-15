ceba079f31105ecb25b8f45d0b17ddc03eb986ce
megazordcarioca <josiasdsj1@gamail.com>
2020-11-15 01:09:25 -0300

    aplicando-front-geral

--- proj_geren_tcc/settings.py
+++ proj_geren_tcc/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'assets/img'),
     os.path.join(BASE_DIR, 'assets/css'),
     os.path.join(BASE_DIR, 'assets/login') #static login files
 )
